90311b309d2012a7991566e52fbac48fd6e6685b
Kazım Etiksan <kazim@pikselmutfak.com>
2023-09-29 11:19:25 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require('./db/mongoose');
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path')
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,15 @@ app.use(bodyParser.json());
 
 const routeUser = require('./routes/user');
 app.use('/api',routeUser);
+
+const publicPath = path.join(__dirname, '..', 'build')
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+
+    const indexPath = path.join(publicPath, 'index.html')
+    res.sendFile(indexPath)
+})
 
 app.listen(8080, () => {
     console.log('started');
